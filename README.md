acae71d64382fa85f8d8b839c42f05869bd36dd5
Gbekeloluwa Olufotebi Simeon <olufotebigbeke@gmail.com>
2017-05-21 17:42:14 +0000

    first draft

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # StoreReporter
+Create an Ecommerce dashboard with Angular 4
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.4.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SummaryCardComponent } from './summary-card/summary-card.component';
+
+import { StoreService } from './store.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryCardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [StoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
